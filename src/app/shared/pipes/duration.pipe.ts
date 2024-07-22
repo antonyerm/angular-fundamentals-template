602dc86f56a776b703b302d8111a762f749e8262
Yermolayev, Anton <anton.yermolayev@outlook.com>
2024-07-22 20:58:18 +0500

    Module 3. Create pipe transformations of duration and date

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,13 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'duration'
 })
-export class DurationPipe {
-    // Add your code here
+export class DurationPipe implements PipeTransform {
+    transform(value: number): string {
+        const hours = Math.floor(value / 60);
+        const minutes = value % 60;
+        const hoursString = hours > 1 ? 'hours' : 'hour';
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${hoursString}`;
+    }
 }
